37914916d4e3f137da1a64578d44e702072df2cc
Jungwonje <study01jwj01@gmail.com>
2024-11-22 16:04:24 +0900

    수정

--- ShootingGame_wonje/src/ShootingGame1.java
+++ ShootingGame_wonje/src/ShootingGame1.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 
 // 네프 12주차 체크포인트
 // 저장확인
-// 3333
+// 3333... cnrkfdfd
 public class ShootingGame1 extends JPanel implements ActionListener, KeyListener {
     private Timer timer;
     private Image playerImage, backgroundImage, monsterImage, missileImage, monsterWeapon;
